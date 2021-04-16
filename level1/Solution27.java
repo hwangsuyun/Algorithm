d046c7f213117079bf63b01aac04730fe830df7d
hwangsuyun@naver.com <hwangsuyun@naver.com>
2021-04-17 00:37:08 +0900

    Solution27.java (2021.04.17 syhwang)

---
+++ level1/Solution27.java
@@ -0,0 +1,13 @@
+package programmers.level1;
+
+// Â¦¼ö¿Í È¦¼ö
+public class Solution27 {
+	public static void main(String[] args) {
+		int num = 4;
+		solution(num);
+	}
+	
+	public static String solution(int num) {
+		return num % 2 == 0 ? "Even" : "Odd";
+	}
+}
