8d08551034a50fe259307d62acdd778794733f35
hwangsuyun@naver.com <hwangsuyun@naver.com>
2025-07-30 21:56:30 +0900

    Solution219.java (2025.07.30 syhwang)

---
+++ level0/Solution219.java
@@ -0,0 +1,13 @@
+package com.example.algorithm.level0;
+
+import java.util.Scanner;
+
+// 덧셈식 출력하기
+public class Solution219 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a + " + " + b + " = " + (a + b));
+    }
+}
