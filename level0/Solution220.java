3cbfece96bfe60b18307dde5a9a5d2af1bdb6a94
hwangsuyun@naver.com <hwangsuyun@naver.com>
2025-07-31 20:43:19 +0900

    Solution220.java (2025.07.31 syhwang)

---
+++ level0/Solution220.java
@@ -0,0 +1,8 @@
+package com.example.algorithm.level0;
+
+// 특수문자 출력하기
+public class Solution220 {
+    public static void main(String[] args) {
+        System.out.println("!@#$%^&*(\\'\"<>?:;");
+    }
+}
