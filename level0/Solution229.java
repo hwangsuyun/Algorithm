f887c0934fc6f15322f37c081e4f09a3d1984c1f
hwangsuyun@naver.com <hwangsuyun@naver.com>
2025-08-09 23:17:27 +0900

    Solution229.java (2025.08.09 syhwang)

---
+++ level0/Solution229.java
@@ -0,0 +1,26 @@
+package com.example.algorithm.level0;
+
+import java.util.Scanner;
+
+// [PCCE 기출문제] 4번 / 저축
+public class Solution229 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int start = sc.nextInt();
+        int before = sc.nextInt();
+        int after = sc.nextInt();
+
+        int money = start;
+        int month = 1;
+        while (money < 70) {
+            money +=  before;
+            month++;
+        }
+        while (money < 100) {
+            money += after;
+            month++;
+        }
+
+        System.out.println(month);
+    }
+}
