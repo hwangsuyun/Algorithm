d25fda1dd18f52d1fd3e443adf109bcf92e69a70
hwangsuyun@naver.com <hwangsuyun@naver.com>
2025-08-11 19:56:37 +0900

    Solution231.java (2025.08.11 syhwang)

---
+++ level0/Solution231.java
@@ -0,0 +1,16 @@
+package com.example.algorithm.level0;
+
+import java.util.Scanner;
+
+// [PCCE 기출문제] 2번 / 피타고라스의 정리
+public class Solution231 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int c = sc.nextInt();
+
+        int b_square = c * c - a * a;
+
+        System.out.println(b_square);
+    }
+}
