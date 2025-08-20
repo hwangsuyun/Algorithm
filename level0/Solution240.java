8b087af31d41d0028488a7b5ff4c3f0297bc6ec0
hwangsuyun@naver.com <hwangsuyun@naver.com>
2025-08-20 20:17:12 +0900

    Solution240.java (2025.08.20 syhwang)

---
+++ level0/Solution240.java
@@ -0,0 +1,35 @@
+package com.example.algorithm.level0;
+
+// [PCCE 기출문제] 8번 / 닉네임 규칙
+public class Solution240 {
+    public static void main(String[] args) {
+        //String nickname = "WORLDworld"; // "VV0RLDvv"
+        String nickname = "GO"; // "G0oo"
+
+        System.out.println(solution(nickname));
+    }
+
+    public static String solution(String nickname) {
+        String answer = "";
+        for (int i = 0; i < nickname.length(); i++) {
+            if (nickname.charAt(i) == 'l') {
+                answer += "I";
+            } else if (nickname.charAt(i) == 'w') {
+                answer += "vv";
+            } else if (nickname.charAt(i) == 'W') {
+                answer += "VV";
+            } else if (nickname.charAt(i) == 'O') {
+                answer += "0";
+            } else {
+                answer += nickname.charAt(i);
+            }
+        }
+        if (answer.length() < 3) {
+            answer += "o".repeat(4 - answer.length());
+        }
+        if (answer.length() > 8) {
+            answer = answer.substring(0, 8);
+        }
+        return answer;
+    }
+}
